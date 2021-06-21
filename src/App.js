3c8864f70f3119c6f0b100f46ae231f6cecf1de8
Jorge Martin Duarte Lopez <60454925+jorge1998duarte@users.noreply.github.com>
2021-06-21 10:34:41 -0500

    Convocatoria Cerrada.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './assets/styles.css'
 import Header from './singles/Header';
 import Captura from './rutas/Captura';
+import CierreConvocatoria from './rutas/CierreConvocatoria';
 import Administracion from './rutas/Administracion';
 
 /* CONTEXT */
@@ -32,7 +33,8 @@ const App = () => {
               <Header cierre={cierre} />
               <hr className='gradiente' />
               <Switch>
-                <Route exact path="/" component={Captura} />
+                {/* <Route exact path="/" component={Captura} /> */}
+                <Route exact path="/" component={CierreConvocatoria} />
                 <Route exact path="/dashboard" component={Administracion} />
                 <Route exact path="/pdf" component={PDF} />
               </Switch>
