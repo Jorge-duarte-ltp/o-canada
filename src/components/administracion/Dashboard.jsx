9440957791aab8a1864b8e88d1bee81c280a6237
Jorge Martin Duarte Lopez <60454925+jorge1998duarte@users.noreply.github.com>
2021-07-29 17:08:14 -0500

    cambios aplicados

--- src/components/administracion/Dashboard.jsx
+++ src/components/administracion/Dashboard.jsx
@@ -1,61 +1,60 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import sessionContext from "../../context/session/sessionContext";
-import SideBar from './SideBar';
-import RevisionDocumentacion from '../regionales/RevisionDocumentacion';
+import SideBar from "./SideBar";
+import RevisionDocumentacion from "../regionales/RevisionDocumentacion";
 
 // import AlertError from '../../singles/AlertError';
 
-import MesaAyuda from '../..//components/mesa_ayuda/MesaAyuda';
+import MesaAyuda from "../..//components/mesa_ayuda/MesaAyuda";
 // import S9 from '../estatales/S9';
-import TablaEstatales from '../estatales/TablaEstatales';
-import TablaBrigadas from '../brigadas/TablaBrigadas';
+import TablaEstatales from "../estatales/TablaEstatales";
+import TablaBrigadas from "../brigadas/TablaBrigadas";
 
-{/**/}
+{
+  /**/
+}
 
 const Dashboard = ({ userPorfile }) => {
+  const [showSection, setShowSection] = useState({
+    regionales: false,
+    estatales: false,
+    mesa_ayuda: false,
+    manifiesto: false,
+    brigadas: false,
+  });
 
-    const [showSection, setShowSection] = useState({
-        'regionales': false,
-        'estatales': false,
-        'mesa_ayuda': false,
-        'manifiesto': false,
-        'brigadas': false,
-    })
+  const [toggled, setToggled] = useState(true);
 
-    const [toggled, setToggled] = useState(true)
+  const handleToggle = () => {
+    setToggled(!toggled);
+  };
 
-    const handleToggle = () => {
-        setToggled(!toggled)
-    }
-
-
-    return (
-        <div className={`d-flex ${(toggled) ? null : 'toggled'}`} id="wrapper">
-            {/* SIDEBAR */}
-            <SideBar
-                showSection={showSection}
-                setShowSection={setShowSection}
-                title={'Secciones'}
-                porfileSections={userPorfile}
-            />
-            {/* CONTENIDO DASHBOARD*/}
-            <div id="page-content-wrapper">
-                <label className="switch">
-                    <input type="checkbox" checked={toggled} onChange={handleToggle} />
-                    <div className="slider"></div>
-                </label>
-                <div className="container-fluid">
-                    {showSection.regionales && <RevisionDocumentacion />}
-                    {/* {showSection.estatales && <S9 />} */}
-                    {showSection.estatales && <TablaEstatales />}
-                    {showSection.mesa_ayuda && <MesaAyuda />}
-                    {/* {showSection.manifiesto && } */}
-                    {showSection.brigadas && <TablaBrigadas />}
-
-                </div>
-            </div>
+  return (
+    <div className={`d-flex ${toggled ? null : "toggled"}`} id="wrapper">
+      {/* SIDEBAR */}
+      <SideBar
+        showSection={showSection}
+        setShowSection={setShowSection}
+        title={"Secciones"}
+        porfileSections={userPorfile}
+      />
+      {/* CONTENIDO DASHBOARD*/}
+      <div id="page-content-wrapper">
+        <label className="switch">
+          <input type="checkbox" checked={toggled} onChange={handleToggle} />
+          <div className="slider"></div>
+        </label>
+        <div className="container-fluid">
+          {showSection.regionales && <RevisionDocumentacion />}
+          {/* {showSection.estatales && <S9 />} */}
+          {showSection.estatales && <TablaEstatales />}
+          {/* {showSection.manifiesto } */}
+          {showSection.brigadas && <TablaBrigadas />}
+          {showSection.mesa_ayuda && <MesaAyuda />}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
--- src/components/administracion/SideBar.jsx
+++ src/components/administracion/SideBar.jsx
@@ -1,93 +1,119 @@
-import React from 'react'
+import React from "react";
 
 const SideBar = (props) => {
-    const { title = 'Cotejo de documentos', showSection, setShowSection, porfileSections } = props
+  const {
+    title = "Cotejo de documentos",
+    showSection,
+    setShowSection,
+    porfileSections,
+  } = props;
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        const clicked = e.target.value;
+  const handleClick = (e) => {
+    e.preventDefault();
+    const clicked = e.target.value;
 
-        switch (clicked) {
-            case 'regionales':
-                setShowSection({
-                    regionales: true,
-                    estatales: false,
-                    mesa_ayuda: false,
-                    manifiesto: false,
-                    brigadas: false,
-                })
-                break;
-            case 'estatales':
-                setShowSection({
-                    regionales: false,
-                    estatales: true,
-                    mesa_ayuda: false,
-                    manifiesto: false,
-                    brigadas: false,
-                })
-                break;
-            case 'mesa_ayuda':
-                setShowSection({
-                    regionales: false,
-                    estatales: false,
-                    mesa_ayuda: true,
-                    manifiesto: false,
-                    brigadas: false,
-                })
-                break;
-            case 'manifiesto':
-                setShowSection({
-                    regionales: false,
-                    estatales: false,
-                    mesa_ayuda: false,
-                    manifiesto: true,
-                    brigadas: false,
-                })
-                break;
-            case 'brigadas':
-                setShowSection({
-                    regionales: false,
-                    estatales: false,
-                    mesa_ayuda: false,
-                    manifiesto: false,
-                    brigadas: true,
-                })
-                break;
-            default: return;
-        }
+    switch (clicked) {
+      case "regionales":
+        setShowSection({
+          regionales: true,
+          estatales: false,
+          mesa_ayuda: false,
+          manifiesto: false,
+          brigadas: false,
+        });
+        break;
+      case "estatales":
+        setShowSection({
+          regionales: false,
+          estatales: true,
+          mesa_ayuda: false,
+          manifiesto: false,
+          brigadas: false,
+        });
+        break;
+      case "mesa_ayuda":
+        setShowSection({
+          regionales: false,
+          estatales: false,
+          mesa_ayuda: true,
+          manifiesto: false,
+          brigadas: false,
+        });
+        break;
+      case "manifiesto":
+        setShowSection({
+          regionales: false,
+          estatales: false,
+          mesa_ayuda: false,
+          manifiesto: true,
+          brigadas: false,
+        });
+        break;
+      case "brigadas":
+        setShowSection({
+          regionales: false,
+          estatales: false,
+          mesa_ayuda: false,
+          manifiesto: false,
+          brigadas: true,
+        });
+        break;
+      default:
+        return;
     }
-    return (
-        <div className="bg-light border-right" id="sidebar-wrapper">
-            <div className="sidebar-heading">{title}</div>
-            <div className="list-group list-group-flush">
-                {porfileSections.regionales && <button value='regionales' className="list-group-item list-group-item-action bg-light"
-                    onClick={handleClick}
-                >
-                    Regionales
-                </button>}
-                {porfileSections.estatales && <button value='estatales' className="list-group-item list-group-item-action bg-light"
-                    onClick={handleClick}
-                >
-                    Estatales
-                </button>}
-                {porfileSections.mesa_ayuda && <button value='mesa_ayuda' className="list-group-item list-group-item-action bg-light"
-                    onClick={handleClick}
-                >
-                    Mesa
-                 de ayuda</button>}
-                {porfileSections.manifiesto && <button value='manifiesto' className="list-group-item list-group-item-action bg-light"
-                    onClick={handleClick}
-                >
-                    Manifiesto
-                </button>}
-                {porfileSections.brigadas && <button value='brigadas' className="list-group-item list-group-item-action bg-light"
-                    onClick={handleClick}
-                >
-                    Brigadas
-                </button>}
-            </div>
-        </div>
-    );
-}
+  };
+  return (
+    <div className="bg-light border-right" id="sidebar-wrapper">
+      <div className="sidebar-heading">{title}</div>
+      <div className="list-group list-group-flush">
+        {porfileSections.regionales && (
+          <button
+            value="regionales"
+            className="list-group-item list-group-item-action bg-light"
+            onClick={handleClick}
+          >
+            Regionales
+          </button>
+        )}
+        {porfileSections.estatales && (
+          <button
+            value="estatales"
+            className="list-group-item list-group-item-action bg-light"
+            onClick={handleClick}
+          >
+            Estatales
+          </button>
+        )}
+        {porfileSections.manifiesto && (
+          <button
+            value="manifiesto"
+            className="list-group-item list-group-item-action bg-light"
+            onClick={handleClick}
+          >
+            Manifiesto
+          </button>
+        )}
+        {porfileSections.brigadas && (
+          <button
+            value="brigadas"
+            className="list-group-item list-group-item-action bg-light"
+            onClick={handleClick}
+          >
+            Brigadas
+          </button>
+        )}
+        {porfileSections.mesa_ayuda && (
+          <button
+            value="mesa_ayuda"
+            className="list-group-item list-group-item-action bg-light"
+            onClick={handleClick}
+          >
+            Mesa de ayuda
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default SideBar;
