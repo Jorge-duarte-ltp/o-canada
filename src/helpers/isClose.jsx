455e7eb31a32455b4925037c622217fb3926a983
Evan <manuel.peralta@conafor.gob.mx>
2023-07-10 14:57:02 -0600

    Cambio de fechas por variables de entorno

--- src/helpers/isClose.jsx
+++ src/helpers/isClose.jsx
@@ -2,8 +2,8 @@ import dayjs from "dayjs";
 
 const isClose = () => {
 
-  const start_date = "2023-07-11 09:00:00";
-  const end_date = "2023-07-11 11:00:00";
+  const start_date = process.env.REACT_APP_F1_START_DATE;
+  const end_date = process.env.REACT_APP_F1_END_DATE;
 
   const isAfter = dayjs(start_date).isBefore();
   const isBefore = dayjs().isAfter(end_date);
